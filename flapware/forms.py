f96559ea33ad334e78359dd659b18bb22169c6c4
Johnny <johnkcli@hotmail.com>
2023-12-07 06:55:08 +0000

    receive post request

--- flapware/forms.py
+++ flapware/forms.py
@@ -3,8 +3,6 @@ from django import forms
 
 class HomeForm(forms.Form):
     city_or_airport = forms.CharField(label="Home city or airport", min_length=1)
-    direct = forms.BooleanField(required=False, label="Direct only")
-    one_way = forms.BooleanField(required=False, label="One way")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- flapware/views.py
+++ flapware/views.py
@@ -1,10 +1,19 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from flapware.forms import HomeForm
 
 
-def home(request):
+async def home(request):
     form = HomeForm()
+    if request.method == "POST":
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            return render(
+                request,
+                "home.html",
+                {"form": form},
+            )
     return render(
         request,
         "home.html",
