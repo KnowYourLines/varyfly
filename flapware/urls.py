8c3bbe9dd20d86b99c48a58975c1d58d5411c58b
Johnny <johnkcli@hotmail.com>
2023-12-12 08:31:53 +0000

    add trailing slash to urls

--- flapware/urls.py
+++ flapware/urls.py
@@ -3,12 +3,17 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("save-home", views.save_home, name="save_home"),
+    path("save-home/", views.save_home, name="save_home"),
     path(
-        "destinations",
+        "destinations/",
         views.destinations,
         name="destinations",
     ),
-    path("add-cities", views.add_cities, name="add_cities"),
-    path("remove-cities", views.remove_cities, name="remove_cities"),
+    path("add-cities/", views.add_cities, name="add_cities"),
+    path("remove-cities/", views.remove_cities, name="remove_cities"),
+    path(
+        "safety/<str:username>/",
+        views.safety,
+        name="safety",
+    ),
 ]
