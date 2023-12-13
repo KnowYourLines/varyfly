b5b7a0f88872848a193e6f6c7e75a2b32e3ccb01
Johnny <johnkcli@hotmail.com>
2023-12-13 19:58:09 +0000

    rename flapware to veyfly

--- veyfly/apps.py
+++ veyfly/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FlapwareConfig(AppConfig):
+class VeyflyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "veyfly"
